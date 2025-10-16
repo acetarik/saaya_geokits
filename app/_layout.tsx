3ca89240d8914fc75e0602b9d5ae609e5016c29a
acetarik <atariq.bscs21seecs@seecs.edu.pk>
2025-10-16 12:45:07 +0500

    fix: headers fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -57,6 +57,8 @@ export default function RootLayout() {
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
         <Stack.Screen name="land-management" options={{ headerShown: false }} />
         <Stack.Screen name="land-selector" options={{ headerShown: false }} />
+        <Stack.Screen name="account-settings" options={{ headerShown: false }} />
+        <Stack.Screen name="gps-land-selector" options={{headerShown: false}}/>
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
